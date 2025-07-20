742906761dc24afbc9cfa959f4d1617948d228ce
Kevin Lago <kevinthelago@gmail.com>
2025-07-19 21:16:47 -0400

    Initial Commit

---
+++ src/main/java/com/kevinthelago/dotdotdash/DotdotdashServerApplication.java
@@ -0,0 +1,13 @@
+package com.kevinthelago.dotdotdash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotdotdashServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotdotdashServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevinthelago/dotdotdash/DotdotdashServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinthelago.dotdotdash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotdotdashServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
